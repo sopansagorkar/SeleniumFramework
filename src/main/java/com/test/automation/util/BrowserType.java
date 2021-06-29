756b749ec57f42cf3b5f97a6897076c8a8383373
Sopan Sagorkar <sopan.sagorkar@fisglobal.com>
2021-06-29 08:31:37 +0530

    Added Config details

---
+++ src/main/java/com/test/automation/util/BrowserType.java
@@ -0,0 +1,11 @@
+package com.test.automation.util;
+
+public enum BrowserType {
+	chrome("chrome"), safari("safari"), edge("edge"), phantomjs("phantomjs"),firefox("firefox");
+
+	public final String browser;
+
+	private BrowserType(String label) {
+		this.browser = label;
+	}
+}
