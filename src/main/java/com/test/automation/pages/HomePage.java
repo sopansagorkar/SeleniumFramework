0d1a7cbfd3001a72309a51e2f02cad144bca03ce
sopansagorkar <sagorkars@gmail.com>
2022-07-20 16:56:56 +0530

    Updated locators

--- src/main/java/com/test/automation/pages/HomePage.java
+++ src/main/java/com/test/automation/pages/HomePage.java
@@ -9,7 +9,7 @@ public class HomePage extends Base {
 	public void searchHotel(HomeData data) {
 		
 		driver.findElement(By.xpath("//a[contains(text(),'Home')]")).click();
-		driver.findElement(By.xpath("//span[contains(text(),'Search by Hotel or City Name')]")).click();
+		driver.findElement(By.xpath("//input[@role='searchbox']")).click();
 		driver.findElement(By.xpath("//div[@id='select2-drop']/div/input")).sendKeys(data.getDestination());
 		driver.findElement(By.id("checkin")).sendKeys(data.getCheckIn());
 		driver.findElement(By.id("checkout")).sendKeys(data.getCheckOut());
--- src/main/java/com/test/automation/pages/LoginPage.java
+++ src/main/java/com/test/automation/pages/LoginPage.java
@@ -17,15 +17,15 @@ public class LoginPage extends Base {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-		driver.findElement(By.cssSelector(".dropdown-login > #dropdownCurrency")).click();
+		//driver.findElement(By.cssSelector(".dropdown-login > #dropdownCurrency")).click();
 		driver.findElement(By.linkText("Login")).click();
-		driver.findElement(By.name("username")).sendKeys(data.getUserName());
+		driver.findElement(By.name("email")).sendKeys(data.getUserName());
 		driver.findElement(By.name("password")).sendKeys(data.getPassword());
-		driver.findElement(By.xpath("//*[@id='loginfrm']/button")).click();
+		driver.findElement(By.xpath("//span[contains(text(),'Login')]")).click();
 	}
 
 	public void verifyLogin(LoginData data) {
-		driver.findElement(By.cssSelector(".nav-item:nth-child(2) > .nav-link")).click();
+		driver.findElement(By.xpath("//a[@class=' waves-effect' and contains(text(),'My Profile')]")).click();
 		Assert.assertEquals(data.getUserName(),
 				driver.findElement(By.xpath("//input[@name='email']")).getAttribute("value").toString());
 	}
